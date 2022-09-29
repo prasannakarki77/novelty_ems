4cc422cacfdd43ec7fccb51b3b65c10269592f31
prasannakarki77 <prasanna.karki77@gmail.com>
2022-09-29 19:35:50 +0545

    added route protection

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import PrivateRoute from "../ProtectedRoute";
 import Dashboard from "./Dashboard";
 import Employees from "./Employees";
 import Login from "./Login";
@@ -7,9 +8,23 @@ const Body = () => {
   return (
     <div className="body-container">
       <Routes>
-        <Route path="/" element={<Login />}></Route>
-        <Route path="/dashboard" element={<Dashboard />}></Route>
-        <Route path="/employees" element={<Employees />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        ></Route>
+        <Route
+          path="/employees"
+          element={
+            <PrivateRoute>
+              <Employees />
+            </PrivateRoute>
+          }
+        ></Route>
       </Routes>
     </div>
   );
